e029fb07e4135e65329d1d27827165f7e8e74fc9
sarandesigner <138696235+sarandesigner@users.noreply.github.com>
2023-11-20 18:23:08 +0530

    Add files via upload

---
+++ build/precache-manifest.f949f8b605fae0826e748254f1e4a775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883863f529043e1e29de0e3bdc430584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed468f951468cc5ed3b",
+    "url": "/static/css/main.17f771f7.chunk.css"
+  },
+  {
+    "revision": "fa8a73b13bde7164dea8",
+    "url": "/static/js/2.bfe6a21a.chunk.js"
+  },
+  {
+    "revision": "5ed468f951468cc5ed3b",
+    "url": "/static/js/main.7b8cde61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6bb08b1b9feb7042118c10cb54bb99a5",
+    "url": "/static/media/logo.6bb08b1b.png"
+  }
+]);
